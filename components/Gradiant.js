8549568e91670ebcf4412077ebff3b1fa824ccf3
Mohammad Maruf <mohammadmarufgazi@gmail.com>
2023-02-06 15:07:29 +0600

    Somthing change bg

--- components/Gradiant.js
+++ components/Gradiant.js
@@ -1,7 +1,7 @@
 
 function Gradiant() {
   return (
-    <div className=" sm:w-[884px] w-[500px]  sm:h-[884px] h-screen sm:rounded-[999px] absolute top-[-250px] sm:top-[-340px] sm:left-[590px]  left-[10px] sm:blur-[590px] blur-[400px]  sm:bg-opacity-[10%] bg-opacity-[60%] bg-gradient-to-t from-[#D388FF] via-[#D388FF] z-[-10] " />
+    <div className=" sm:w-[884px] w-[200px]  sm:h-[884px] h-screen sm:rounded-[999px] absolute top-[-250px] sm:top-[-340px] sm:left-[590px]  left-[10px] sm:blur-[590px] blur-[400px]  sm:bg-opacity-[10%] bg-opacity-[60%] bg-gradient-to-t from-[#D388FF] via-[#D388FF] z-[-10] " />
   )
 }
 
